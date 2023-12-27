21a1dd903d00f11b78e5c155d5157b7131dd5e59
dental2012141626 <51718942+jdrdionisio@users.noreply.github.com>
2023-12-27 12:23:56 +0000

    Module 3 v1

--- Later.R
+++ Later.R
@@ -19,3 +19,34 @@ Isto permite reutilizar código de outros documentos sem precisar de copiar e co
 
 
 Params definition in Quarto and Run.r fro multiple reports
+
+# Exercício 1
+
+- No dataset covid_inc, calcule a média e desvio padrao da incidencia por ARS.
+
+```{r}
+mean_covid <-covid_inc %>%
+  group_by(ARS) %>% 
+  summarise(
+    mean_Incidencia=mean(Incidencia, na.rm=TRUE),
+    SD_Incidencia=sd(Incidencia, na.rm=TRUE)
+  )
+```
+
+Exercicio
+1. Dataset inpatient_hospital_lite
+2. So queremos metricas de hospitais universitários 
+3. Criar metricas de medias e SD dos doentes saidos so para o mes de janeiro
+4. Hospitais superiores a media global sao codificados como zero, os outros como 1
+
+```{r}
+tipo_esp01 <-inpatient_hospital_lite |>
+  select(tipo_de_especialidade)|>
+  mutate(
+    esp01=ifelse(tipo_de_especialidade=="Especialidade Cirurgica", 0,1)
+  )
+```
+
+
+### Exercício
+Crie a média e a mediana dos doentes seguidos e dias de internamento, por ano para os hospitais de oncologia (agrupados).
