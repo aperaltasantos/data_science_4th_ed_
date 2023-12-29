ddaf643c9a561a2b6a8507991625c60306e6e45b
dental2012141626 <51718942+jdrdionisio@users.noreply.github.com>
2023-12-29 14:35:19 +0000

    minor corrections
    minor

--- Later.R
+++ Later.R
@@ -1,21 +1,3 @@
-## Includes
-
-Reutilizar conteúdo em Quarto é possível usando o código "{{< include >}}". 
-
-Isto permite reutilizar código de outros documentos sem precisar de copiar e colar.
-
-```{r}
-#| echo: true
-
-# ---
-# title: "Relatórios Conjuntos"
-# ---
-# 
-# {{< include Relatório_hospitalA.qmd >}}
-# {{< include Relatório_hospitalB.qmd >}}
-# 
-# Usar os dados e outputs do documentos atuais...
-```
 
 
 Params definition in Quarto and Run.r fro multiple reports
